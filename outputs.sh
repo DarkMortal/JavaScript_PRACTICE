d17dc6fc06d6fe88a612ee2dd1717999186cb8b6
Dark Mortal <chodab74@gmail.com>
2023-05-14 00:04:15 +0530

    Updated output logs

--- outputs.sh
+++ outputs.sh
@@ -1,4 +1,4 @@
-declare -a files=('CommonJS/main.js' 'ES6Module/main.mjs' 'Promises/Streams/streams.js' 'Promises/async_await.js' 'Promises/classes.js' 'Promises/promises.js' 'Typescript/index.js' 'classes.js' 'generators.js' 'iterators.js' 'scopes.js')
+declare -a files=('CommonJS/main.js' 'ES6Module/main.mjs' 'Promises/Streams/streams.js' 'Promises/async_await.js' 'Promises/classes.js' 'Promises/promises.js' 'Typescript/index.js' 'Basics/classes.js' 'Basics/generators.js' 'Basics/iterators.js' 'Basics/scopes.js')
 for i in ${files[@]}
 do
 	echo -e "Output of $i :"
